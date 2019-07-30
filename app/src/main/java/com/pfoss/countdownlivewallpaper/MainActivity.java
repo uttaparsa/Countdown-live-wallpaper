88c4f457f7d265da23551175be47f2687f7f70e5
uttaparsa <salehipour.edu>
2019-07-30 13:05:51 +0430

    Merge branch 'master' of /home/parsa/Parsa/Projects/Android/CountDownLiveWallpaper with conflicts.

--- app/src/main/java/com/pfoss/countdownlivewallpaper/MainActivity.java
+++ app/src/main/java/com/pfoss/countdownlivewallpaper/MainActivity.java
@@ -9,7 +9,6 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -36,19 +35,20 @@ public class MainActivity extends AppCompatActivity {
         backgroundImageView = findViewById(R.id.backgroundImageView);
         initializeToolbar();
         timersSharedPreferences = this.getSharedPreferences("com.pfoss.countdownlivewallpaper", Context.MODE_PRIVATE);
-        loadMain();
+
         Log.i("MAIN", "creating main");
+
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        loadMain();
         Log.i("MAIN", "resuming main");
     }
 
     private void initializeToolbar() {
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar =  findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
 
@@ -99,39 +99,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    protected void loadMain() {//MUST CHANGE THIS NAME
-        timerRecords = RecordManager.fetchRecords(timersSharedPreferences);
-        if (timerRecords.isEmpty()) {
-            Log.i("LOADMAIN", "no timers exist right now");
-            backgroundImageView.setVisibility(View.INVISIBLE);
-        } else {
-            findPriorTimerToShow();
-        }
-    }
-
-    private void findPriorTimerToShow() {
-
-        for (int i = 0; i < timerRecords.size(); i++) {
-            Log.i("findPriorTimerToShow", "saw a timer in for loop ");
-            if (timerRecords.get(i).isPriorToShow()) {
-                Log.i("findPriorTimerToShow", "showing " + i + " th timer");
-                showTimer(timerRecords.get(i));
-            }
-        }
-
-    }
-
-    private void showTimer(TimerRecord timerRecord) {
-        currentRecord = timerRecord;
-        if (timerRecord.hasImage()) {
-            showRecordImage(timerRecord);
-        } else {
-            backgroundImageView.setVisibility(View.INVISIBLE);
-        }
-        Log.i("showTimer", "trying to show : " + timerRecord.toString());
-    }
-
-
     private void showRecordImage(TimerRecord record) {
         try {
             backgroundImageView.setImageBitmap(record.getBitmap());
@@ -148,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Main-options", "Deleting a record");
         RecordManager.deleteRecord(timersSharedPreferences, timerRecords, currentRecord);
         refreshPreferences();
-        loadMain();
+
     }
 
 }
